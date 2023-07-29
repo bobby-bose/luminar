bb0be88026ccf35cbd6aa53c374328d9a9210eda
Anumol03 <122881137+Anumol03@users.noreply.github.com>
2023-07-30 01:06:37 +0530

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -7,18 +7,14 @@ sudo pkill -f runserver
 # sudo pkill -f tailwind
 # sudo pkill -f node
 
-cd /home/ubuntu/django-aws_cicd/
+cd /home/ubuntu/luminar/
 
 # activate virtual environment
 python3 -m venv venv
 source venv/bin/activate
 
-install requirements.txt
-pip install -r /home/ubuntu/django-aws_cicd/requirements.txt
+# install requirements.txt
+pip install -r /home/ubuntu/luminar/requirements.txt
 
 # run server
-<<<<<<< HEAD
 screen -d -m python3 manage.py runserver 0:8000
-=======
-screen -d -m python3 manage.py runserver 0:8000
->>>>>>> 9b032624ebbc1cb897feae06dd9936a2c25a05a6
