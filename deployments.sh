7d1fe1bd6a14bd6fe354e1809403d94b8f597b47
Danwand <45515141+DanBrown47@users.noreply.github.com>
2023-08-01 11:06:20 +0530

    Remove collect static

--- deployments.sh
+++ deployments.sh
@@ -3,6 +3,5 @@ git pull origin master
 pip3 install -r requirements.txt
 python3 manage.py makemigrations
 python3 manage.py migrate
-python3 manage.py collectstatic
 sudo systemctl restart nginx
 sudo systemctl restart gunicorn
