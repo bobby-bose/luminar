427472fc5624e7faa7ae11c81f0fab690924ea4f
Danwand <45515141+DanBrown47@users.noreply.github.com>
2023-08-01 10:52:25 +0530

    Remove sudo

--- deployments.sh
+++ deployments.sh
@@ -1,6 +1,6 @@
 #!/bin/sh     
-sudo git pull origin master
-sudo pip3 install -r requirements.txt
+git pull origin master
+pip3 install -r requirements.txt
 python3 manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py collectstatic
